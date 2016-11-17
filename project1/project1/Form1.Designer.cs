a90cb60a750d2ab2a9a4e5ad2453569a0f5934e9
evilaction260895 <sergey_m10@mail.ru>
2016-11-17 22:29:23 +0300

    Change Back Color to Silver

--- project1/project1/Form1.Designer.cs
+++ project1/project1/Form1.Designer.cs
@@ -91,6 +91,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.BackColor = System.Drawing.Color.Silver;
             this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.textBox3);
@@ -102,14 +103,13 @@
             this.tabPage1.Size = new System.Drawing.Size(573, 399);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(573, 399);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
